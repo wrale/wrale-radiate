2a740dc34d409bd0d72342a32ff9e06d11275113
JMDOTS <contact@jmdots.com>
2024-12-05 06:29:22 -0800

    Simplify Next.js config for edge runtime support

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -2,13 +2,6 @@
 const nextConfig = {
   experimental: {
     serverActions: true
-  },
-  webpack: (config) => {
-    config.externals.push({
-      'utf-8-validate': 'commonjs utf-8-validate',
-      'bufferutil': 'commonjs bufferutil',
-    })
-    return config
   }
 }
 
