da507ebb1f62f036e4c381ecab3aa4323ed70d17
JMDOTS <contact@jmdots.com>
2024-12-05 08:58:51 -0800

    Update Next.js config for WebSocket support

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, { webpack, isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        'utf-8-validate': 'commonjs utf-8-validate',
+        'bufferutil': 'commonjs bufferutil',
+      })
+    }
+    return config
+  },
+  experimental: {
+    serverActions: true
+  }
+}
 
 module.exports = nextConfig
