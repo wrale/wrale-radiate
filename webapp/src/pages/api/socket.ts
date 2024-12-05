41e87389e3f6ae0060eab3a2b770805bcac0c45a
JMDOTS <contact@jmdots.com>
2024-12-05 10:29:10 -0800

    Simplify Socket.IO server implementation

--- webapp/src/pages/api/socket.ts
+++ webapp/src/pages/api/socket.ts
@@ -1,29 +1,54 @@
-import { Server as NetServer } from 'http'
-import { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next'
 import { Server as SocketIOServer } from 'socket.io'
-import { getSocketIO } from '@/lib/socket'
-
-interface SocketServer extends NetServer {
-  io?: SocketIOServer
-}
-
-interface ResponseWithSocket extends NextApiResponse {
-  socket: any
-}
-
-export default function handler(req: NextApiRequest, res: ResponseWithSocket) {
-  if (!res.socket.server.io) {
-    const httpServer = res.socket.server as SocketServer
-    httpServer.io = getSocketIO()
-    httpServer.io.attach(httpServer)
-    console.log('[Socket.IO] Server initialized')
-  }
-
-  res.end()
-}
+import type { Server as NetServer } from 'http'
+import type { Socket } from 'socket.io'
 
 export const config = {
   api: {
     bodyParser: false
   }
 }
+
+type NextApiResponseServerIO = NextApiResponse & {
+  socket: {
+    server: NetServer & {
+      io: SocketIOServer
+    }
+  }
+}
+
+const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIO) => {
+  if (!res.socket.server.io) {
+    console.log('Initializing Socket.IO...')
+    const io = new SocketIOServer(res.socket.server, {
+      path: '/api/socket',
+      addTrailingSlash: false,
+      cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+      },
+      transports: ['polling', 'websocket']
+    })
+
+    io.on('connection', (socket: Socket) => {
+      console.log('Client connected:', socket.id)
+
+      socket.on('health', (data) => {
+        console.log('Health update from', socket.id, ':', data)
+      })
+
+      socket.emit('connected', { id: socket.id })
+
+      socket.on('disconnect', (reason) => {
+        console.log('Client disconnected:', socket.id, reason)
+      })
+    })
+
+    res.socket.server.io = io
+  } else {
+    console.log('Socket.IO already running')
+  }
+  res.end()
+}
+
+export default ioHandler
