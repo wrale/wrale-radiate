a2d2cb846e3dff6988f6d6f1254ea1403aa94dc1
JMDOTS <contact@jmdots.com>
2024-12-05 10:21:49 -0800

    Update socket API route with proper typing

--- webapp/src/pages/api/socket.ts
+++ webapp/src/pages/api/socket.ts
@@ -3,11 +3,19 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { Server as SocketIOServer } from 'socket.io'
 import { getSocketIO, config as socketConfig } from '@/lib/socket'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (!res.socket?.server?.io) {
-    const httpServer: NetServer = res.socket.server as any
-    res.socket.server.io = getSocketIO()
-    res.socket.server.io.attach(httpServer)
+interface SocketServer extends NetServer {
+  io?: SocketIOServer
+}
+
+interface ResponseWithSocket extends NextApiResponse {
+  socket: any
+}
+
+export default function handler(req: NextApiRequest, res: ResponseWithSocket) {
+  if (!res.socket.server.io) {
+    const httpServer = res.socket.server as SocketServer
+    httpServer.io = getSocketIO()
+    httpServer.io.attach(httpServer)
     console.log('[Socket.IO] Server initialized')
   }
 
