57a1463a6b7517386d4f081427305df5acf33d0d
JMDOTS <contact@jmdots.com>
2024-12-05 19:51:39 -0800

    Fix WebSocket server ownership and CORS issues

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,12 +7,12 @@ use axum::{
     response::{IntoResponse, Response},
     routing::get,
     Json, Router,
-    http::{HeaderValue, Method},
+    http::{Method, HeaderValue},
 };
 use axum_extra::headers::{AccessControlAllowOrigin, HeaderMapExt};
 use futures::{sink::SinkExt, stream::StreamExt};
 use serde_json::Value;
-use tokio::sync::broadcast;
+use tokio::sync::{broadcast, mpsc};
 use tower_http::cors::CorsLayer;
 use tracing_subscriber::fmt::format::FmtSpan;
 
@@ -43,9 +43,9 @@ async fn main() {
 
     // Configure CORS
     let cors = CorsLayer::new()
-        .allow_methods([Method::GET, Method::POST])
-        .allow_origin("*".parse::<HeaderValue>().unwrap())
+        .allow_methods(vec![Method::GET, Method::POST])
         .allow_headers(tower_http::cors::Any)
+        .allow_origin(tower_http::cors::Any)
         .expose_headers(tower_http::cors::Any);
 
     // Build our application
@@ -110,6 +110,9 @@ async fn handle_socket(socket: WebSocket, state: SharedState) {
         tracing::info!("Client connected: {}", client_id);
     }
 
+    // Create a channel for sending messages to the WebSocket
+    let (tx_socket, mut rx_socket) = mpsc::channel(32);
+
     // Subscribe to broadcast
     let mut rx = state.tx.subscribe();
 
@@ -121,29 +124,39 @@ async fn handle_socket(socket: WebSocket, state: SharedState) {
     });
     
     if let Ok(msg) = serde_json::to_string(&welcome_msg) {
-        if let Err(e) = sender.send(Message::Text(msg)).await {
-            tracing::error!("Error sending welcome message: {}", e);
-        }
+        let _ = tx_socket.send(Message::Text(msg)).await;
     }
 
+    // Start sender task
+    let send_task = tokio::spawn(async move {
+        while let Some(message) = rx_socket.recv().await {
+            if let Err(e) = sender.send(message).await {
+                tracing::error!("Error sending message: {}", e);
+                break;
+            }
+        }
+    });
+
     // Start heartbeat task
+    let tx_socket_heartbeat = tx_socket.clone();
     let client_id_heartbeat = client_id.clone();
     let heartbeat_task = tokio::spawn(async move {
         let mut interval = tokio::time::interval(tokio::time::Duration::from_secs(30));
         loop {
             interval.tick().await;
-            if let Err(e) = sender.send(Message::Ping(vec![])).await {
+            if let Err(e) = tx_socket_heartbeat.send(Message::Ping(vec![])).await {
                 tracing::error!("Error sending ping to {}: {}", client_id_heartbeat, e);
                 break;
             }
         }
     });
 
-    // Handle incoming messages
-    let mut send_task = tokio::spawn(async move {
+    // Start broadcast listener task
+    let tx_socket_broadcast = tx_socket.clone();
+    let broadcast_task = tokio::spawn(async move {
         while let Ok(msg) = rx.recv().await {
-            if let Err(e) = sender.send(Message::Text(msg)).await {
-                tracing::error!("Error sending message: {}", e);
+            if let Err(e) = tx_socket_broadcast.send(Message::Text(msg)).await {
+                tracing::error!("Error forwarding broadcast: {}", e);
                 break;
             }
         }
@@ -152,6 +165,7 @@ async fn handle_socket(socket: WebSocket, state: SharedState) {
     // Handle messages from client
     let tx = state.tx.clone();
     let client_id_clone = client_id.clone();
+    let tx_socket_recv = tx_socket.clone();
     let mut recv_task = tokio::spawn(async move {
         while let Some(Ok(msg)) = receiver.next().await {
             match msg {
@@ -181,7 +195,7 @@ async fn handle_socket(socket: WebSocket, state: SharedState) {
                     }
                 }
                 Message::Ping(data) => {
-                    if let Err(e) = sender.send(Message::Pong(data)).await {
+                    if let Err(e) = tx_socket_recv.send(Message::Pong(data)).await {
                         tracing::error!("Error sending pong: {}", e);
                         break;
                     }
@@ -194,17 +208,25 @@ async fn handle_socket(socket: WebSocket, state: SharedState) {
 
     // Wait for any task to finish
     tokio::select! {
-        _ = (&mut send_task) => {
+        _ = send_task => {
             recv_task.abort();
+            broadcast_task.abort();
             heartbeat_task.abort();
         },
-        _ = (&mut recv_task) => {
+        _ = recv_task => {
             send_task.abort();
+            broadcast_task.abort();
+            heartbeat_task.abort();
+        },
+        _ = broadcast_task => {
+            send_task.abort();
+            recv_task.abort();
             heartbeat_task.abort();
         },
         _ = heartbeat_task => {
             send_task.abort();
             recv_task.abort();
+            broadcast_task.abort();
         }
     };
 
